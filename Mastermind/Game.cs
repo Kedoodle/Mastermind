c7d2e1252e0942815565043a44dd004d078a82b2
Keddy Shen <keddy.shen@myob.com>
2018-11-22 16:59:41 +1300

    Takes input string array and outputs string array of "Black" accordingly

--- Mastermind/Game.cs
+++ Mastermind/Game.cs
@@ -4,14 +4,24 @@ using System.Collections.Generic;
 namespace Mastermind {
     public class Game {
 
-        private static List<string> PossibleColours = new List<string>(){"Red", "Blue", "Green", "Orange", "Purple", "Yellow"};
-        private List<string> GameColours;
+        private static readonly string[] PossibleColours = {"Red", "Blue", "Green", "Orange", "Purple", "Yellow"};
+        private static readonly string[] GameColours = new string[4];
         public Game() {
             var r = new Random();
             for (var i = 0; i < 4; i++) {
-                var randomIndex = r.Next(PossibleColours.Count);
-                var colour = PossibleColours[randomIndex];
+                var randomIndex = r.Next(PossibleColours.Length);
+                GameColours[i] = PossibleColours[randomIndex];
             }
+        }
+
+        public static IEnumerable<string> mastermind(string[] input) {
+            var output = new List<string>();
+            for (var i = 0; i < 4; i++) {
+                if (input[i] == GameColours[i]) {
+                    output.Add("Black");
+                }
+            }
+            return output.ToArray();
         }
     }
 }
---
+++ MastermindTests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using Mastermind;
+using Xunit;
+
+namespace MastermindTests {
+    public class Mastermind {
+        
+        private Game game;
+        
+        public Mastermind() {
+            game = new Game();  
+        }
+        
+        [Fact]
+        public void TakesStringArrayAndReturns() {
+            var input = new[] {"Red", "Blue", "Green", "Yellow"};
+            var output = Game.mastermind(input);
+            Assert.NotNull(output);
+        }
+        
+        [Fact]
+        public void TakesStringArrayAndReturnsBlackElements() {
+            var input = new[] {"Red", "Blue", "Green", "Yellow"};
+            var expectedSubstring = "Black";
+            var output = Game.mastermind(input);
+            Assert.Contains(expectedSubstring, output);
+        }
+    }
+}
